36b1d55f079a9bd70dc165b13eeb2793de0f3253
Jakub Grajciar <grajciar.jakub@gmail.com>
2017-07-10 12:10:56 +0200

    ODPM-378

--- src/socket.c
+++ src/socket.c
@@ -15,17 +15,25 @@
  *------------------------------------------------------------------
  */
 
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/un.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/uio.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include <socket.h>
 #include <memif.h>
 
-#define memif_min(a,b) a < b ? a : b
+#define memif_min(a,b) ((a < b) ? (a) : (b))
 
 /* sends msg to socket */
 static_fn int
@@ -43,8 +51,8 @@ memif_msg_enq_ack (memif_connection_t *c)
 
     e->msg.type = MEMIF_MSG_TYPE_ACK;
     e->fd = -1;
-    e->next = NULL;
 
+    e->next = NULL;
     if (c->msg_queue == NULL)
     {
         c->msg_queue = e;
@@ -75,8 +83,6 @@ memif_msg_send_hello (memif_connection_t *c)
     strncpy ((char *) h->name, (char *) c->args.instance_name,
             strlen ((char *) c->args.instance_name));
 
-    /* or initialize hello msg and return it to user to create own communication
-        (if there is use case) */
     /* msg hello is not enqueued but sent directly,
          because it is the first msg to be sent */
     return memif_msg_send (c->fd, &msg, -1);
@@ -103,7 +109,6 @@ memif_msg_enq_init (memif_connection_t *c)
         strncpy ((char *) i->secret, (char *) c->args.secret, sizeof (i->secret));
 
     e->next = NULL;
-
     if (c->msg_queue == NULL)
     {
         c->msg_queue = e;
@@ -129,8 +134,8 @@ memif_msg_receive_hello (memif_connection_t *c, memif_msg_t *msg)
         DBG ("incompatible protocol version");
         return -1;
     }
-    /* there will probably be a nested struct c->run containing following variables
-        (this will be used to adjust shared memory information while keeping
+    /* use nested struct c->run containing following variables?
+        (this would be used to adjust shared memory information while keeping
         configured values intact) */
     c->args.num_s2m_rings = memif_min (h->max_s2m_ring + 1,
                                     c->args.num_s2m_rings);
@@ -148,31 +153,37 @@ static_fn int
 memif_msg_receive_init (memif_connection_t *c, memif_msg_t *msg)
 {
     memif_msg_init_t *i = &msg->init;
+    uint8_t err_string[96];
     if (i->version != MEMIF_VERSION)
     {
-        DBG ("unsuported version");
+        DBG ("MEMIF_VER_ERR");
+        strncpy ((char *) err_string, MEMIF_VER_ERR, strlen (MEMIF_VER_ERR));
         goto error;
     }
     if (c->args.interface_id != i->id)
     {
-        DBG ("unmatched interface id");
+        DBG ("MEMIF_ID_ERR");
+        strncpy ((char *) err_string, MEMIF_ID_ERR, strlen (MEMIF_ID_ERR));
         goto error;
     }
 
 
     if (!(c->args.is_master))
     {
-        DBG ("cannot connect to slave");
+        DBG ("MEMIF_SLAVE_ERR");
+        strncpy ((char *) err_string, MEMIF_SLAVE_ERR, strlen (MEMIF_SLAVE_ERR));
         goto error;
     }
     if (c->fd != -1)
     {
-        DBG ("already connected");
+        DBG ("MEMIF_CONN_ERR");
+        strncpy ((char *) err_string, MEMIF_CONN_ERR, strlen (MEMIF_CONN_ERR));
         goto error;
     }
     if (i->mode != c->args.mode)
     {
-        DBG ("mode mismatch");
+        DBG ("MEMIF_MODE_ERR");
+        strncpy ((char *) err_string, MEMIF_MODE_ERR, strlen (MEMIF_MODE_ERR));
         goto error;
     }
     
@@ -185,83 +196,84 @@ memif_msg_receive_init (memif_connection_t *c, memif_msg_t *msg)
         {
             if (strlen ((char *) c->args.secret) != strlen ((char *) i->secret))
                 {
-                    DBG ("incorrect secret");
+                    DBG ("MEMIF_SECRET_ERR");
+                    strncpy ((char *) err_string,
+                        MEMIF_SECRET_ERR, strlen (MEMIF_SECRET_ERR));
                     return -1;
                 }
             r = strncmp ((char *) i->secret, (char *) c->args.secret,
                      strlen ((char *) c->args.secret));
             if (r != 0)
             {
-                DBG ("incorrect secret");
+                    DBG ("MEMIF_SECRET_ERR");
+                    strncpy ((char *) err_string,
+                        MEMIF_SECRET_ERR, strlen (MEMIF_SECRET_ERR));
                 return -1;
             }
         }
         else
         {
-            DBG ("secret required");
+            DBG ("MEMIF_NOSECRET_ERR");
+            strncpy ((char *) err_string,
+                MEMIF_NOSECRET_ERR, strlen (MEMIF_NOSECRET_ERR));
             return -1;
         }     
     }
     return 0;
 
 error:
-    /*memif_msg_send_disconnect (c);*/
+    memif_msg_send_disconnect (c, err_string, 1);
     return -1;
 }
 
-/*
- * Slave
- */
+static_fn int
+memif_msg_receive (memif_connection_t *c)
+{
+    return 0;
+}
 
 int
-memif_slave_conn_fd_read_ready (memif_connection_t *c)
+memif_conn_fd_error (memif_connection_t *c)
 {
-    /* calls memif_msg_receive to handle pending messages on socket */
-    /* memif_msg_receive will call required functions based on "memif protocol" */
+    DBG ("connection fd error");
+    strncpy ((char *) c->remote_disconnect_string, "connection fd error",
+        19);
+    memif_disconnect_internal (c);
+    return 0;
+}
+
+/* calls memif_msg_receive to handle pending messages on socket */
+int
+memif_conn_fd_read_ready (memif_connection_t *c)
+{
+    int rv = 0;
+    rv = memif_msg_receive (c);
+    if (rv < 0)
+    {
+        memif_disconnect_internal (c);
+    }
     return 0;
 }
 
 /* get msg from msg queue buffer and send it to socket */
 int
-memif_slave_conn_fd_write_ready (memif_connection_t *c)
+memif_conn_fd_write_ready (memif_connection_t *c)
 {
+    int rv = 0; 
+    if ((c->flags & MEMIF_CONNECTION_FLAG_WRITE) == 0)
+        return rv;
+
     memif_msg_queue_elt_t *e = c->msg_queue;
+    if (e == NULL)
+        return rv;
+
     c->msg_queue = c->msg_queue->next;
   
-    int rv = 0; 
-    /* TODO: check if write is possible */
+    c->flags &= ~MEMIF_CONNECTION_FLAG_WRITE;
  
     rv = memif_msg_send (c->fd, &e->msg, e->fd);
     free(e);
     return rv;
-}
-
-int
-memif_slave_conn_fd_error (memif_connection_t *c)
-{
-    return 0;
-}
-
-/*
- * Master
- */
-
-int
-memif_master_conn_fd_read_ready (memif_connection_t *c)
-{
-    return 0;
-}
-
-int
-memif_master_conn_fd_write_ready (memif_connection_t *c)
-{
-    return 0;
-}
-
-int
-memif_master_conn_fd_error (memif_connection_t *c)
-{
-    return 0;
 }
 
 int
@@ -271,6 +283,8 @@ memif_conn_fd_accept_ready (memif_connection_t *c)
     struct sockaddr_un client;
     int conn_fd;
 
+    DBG ("accept called");
+
     addr_len = sizeof (client);
     conn_fd = accept (c->fd, (struct sockaddr *) &client, (socklen_t *) &addr_len);
 
@@ -279,10 +293,12 @@ memif_conn_fd_accept_ready (memif_connection_t *c)
         DBG ("accept fd %d", c->fd);
         return -1;
     }
+    DBG ("accept fd %d", c->fd);
+    DBG ("conn fd %d", conn_fd);
 
-    c->read_fn = memif_master_conn_fd_read_ready;
-    c->write_fn = memif_master_conn_fd_write_ready;
-    c->error_fn = memif_master_conn_fd_error;
+    c->read_fn = memif_conn_fd_read_ready;
+    c->write_fn = memif_conn_fd_write_ready;
+    c->error_fn = memif_conn_fd_error;
     c->fd = conn_fd;
 
     int e = memif_msg_send_hello (c);
